86389db641f4f525ecf95972d0b53c88001a62aa
Delarobbia <carecarebamerto@gmail.com>
2022-09-07 16:44:33 +0200

    Create tema1.java

---
+++ Ejercicios/tema1.java
@@ -0,0 +1,16 @@
+package Ejercicios;
+
+public class tema1 {
+    public static void main(String[] args) {
+        int a =5;
+        long b = 12000;
+        double c = 5.6;
+        boolean d=true;
+        String e = "Hola mundo";
+        
+    System.out.println("El valor de tus variables son:\n int: "+a+"\nlong: "+b+"\ndouble: "+c+"\nboolean: "+d+"\nstring: "+e);
+    
+
+    }
+    
+}
